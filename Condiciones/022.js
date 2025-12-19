03a080ec332fdc9b27deba682cf3a6c41a400b21
Konra <denapoliconra@gmail.com>
2025-12-19 03:57:00 -0300

    Add files via upload

---
+++ Condiciones/022.js
@@ -0,0 +1,32 @@
+
+const passw = 4672;
+
+if(passw == 4672) {
+    console.log("Bienvenido.")
+} else {
+    console.log("Contraseña incorrecta")
+}
+
+// (=) Sirve para declarar
+// (==) Para comparar si los valores son iguales
+// (===) Para comprobar si el valor y el tipo de dato son iguales
+
+const puntaje = 10;
+
+if(puntaje != 10) {
+    console.log("No cumple el puntaje necesario")
+}
+if(puntaje > 10) {
+    console.log("El puntaje es muy alto")
+}
+if(puntaje < 10) {
+    console.log("El puntaje es muy bajo")
+}
+
+const n1 = 4;
+const n2 = 5;
+if(n1 > n2) {
+    console.log(`${n1} es mayor a ${n2}`)
+} else {
+    console.log(`${n2} es mayor a ${n1}`)
+}
